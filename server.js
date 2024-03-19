890565d8f10103bcc60166741647029170ce91bd
Louis Stafford <louiststafford@gmail.com>
2024-03-19 19:50:58 +0000

    Update server.js port

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const PORT = 2999;
+const PORT = 3001;
 
 const app = express();
-app.listen(PORT, ()=> {console.log('App listening at Port 2999!')});
+app.listen(PORT, ()=> {console.log('App listening at Port 3001!')});
