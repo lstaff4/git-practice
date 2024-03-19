f8b88886a1892c07e59adc90bcafb0459c8d72c0
Warren Uhrich <hello@warren.codes>
2024-03-19 14:08:59 -0600

    Add 'hello name' route to server.js

--- server.js
+++ server.js
@@ -3,3 +3,14 @@ const PORT = 3001;
 
 const app = express();
 app.listen(PORT, ()=> {console.log('App listening at Port 3001!')});
+
+app.get('/hello/:name', (req, res) => {
+  const name = req.params.name;
+  res.status(200).end(`<!DOCTYPE html>
+    <html>
+      <head><title>Hello, ${name}!</title></head>
+      <body><h1>Hello, ${name}!</h1></body>
+    </html>
+  `);
+});
+
