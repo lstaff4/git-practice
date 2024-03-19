9c7c40e41c21706e08a1224c22a6ce539aa9b1ce
Louis Stafford <louiststafford@gmail.com>
2024-03-19 20:03:18 +0000

    Add ejs, add hello world route

--- server.js
+++ server.js
@@ -2,4 +2,10 @@ const express = require('express');
 const PORT = 3001;
 
 const app = express();
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('helloworld.ejs');
+})
+
 app.listen(PORT, ()=> {console.log('App listening at Port 3001!')});
